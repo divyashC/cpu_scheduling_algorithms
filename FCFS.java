3c7b4944bce971fe984fa4302c011d2602bb2f4c
Divyash Chhetri <divyashchhetri@gmail.com>
2022-03-26 03:48:12 +0600

    FCFS Update

--- FCFS.java
+++ FCFS.java
@@ -3,71 +3,68 @@
     Date - 26th March 2022
     Time - 1:11 AM
 */
+
 import java.util.Scanner;
 
 public class FCFS {
 
-    // method to print single digits with 0 (example: 1,2,3 as 01,02,03)
-	public static String format(int n) {
-		return (n < 10 ? "0" : "") + n;
-	}
-
     // method to add all items in an int array
-	public static float sum(int[] arr) {
-		float sum = 0;
-		for( int i : arr) {
-    		sum += i;
-		}
-		return sum;
-	}
+    public static float sum(int[] arr) {
+        float sum = 0;
+        for (int i: arr) {
+            sum += i;
+        }
+        return sum;
+    }
 
     // main method
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        
+
         // getting total number of processes from user
-		System.out.print("Enter Number of Processes: ");
-		int total_p = sc.nextInt();
+        System.out.print("Enter Number of Processes: ");
+        int total_p = sc.nextInt();
 
-		// declaring all required arrays
-		int bt[] = new int[total_p]; // burst time
-		int at[] = new int[total_p]; // arrival time
-		int pNo[] = new int [total_p]; // process number
+        // declaring all required arrays
+        int bt[] = new int[total_p]; // burst time
+        int at[] = new int[total_p]; // arrival time
+        int pNo[] = new int[total_p]; // process number
         int ct[] = new int[total_p]; // completion time    
-		int tat[] = new int[total_p]; // turnaround time
-		int wt[] = new int[total_p]; // waiting time
+        int tat[] = new int[total_p]; // turnaround time
+        int wt[] = new int[total_p]; // waiting time
 
-		System.out.println("\n- - - -  * Enter Burst Time and Arrival Time for each Process * - - - -");
+        System.out.println("\nEnter Burst Time and Arrival Time for each Process");
 
-		// getting Arrival time from user
-		System.out.println("\nEnter Arrival Time for all Processes: e.g. P1 P2 P3 P4 P5 (separate with spaces)");
+        // getting Arrival time from user
+        System.out.println("\nEnter Arrival Time for all Processes: e.g. P1 P2 P3 P4 P5 (separate with spaces)");
+        for (int i = 0; i < total_p; i++) {
+            pNo[i] = i + 1;
+            at[i] = sc.nextInt();
+        }
+
+        // getting Burst time from user
+        System.out.println("\nEnter Burst Time for all Processes: e.g. P1 P2 P3 P4 P5 (separate with spaces)");
+        for (int i = 0; i < total_p; i++) {
+            bt[i] = sc.nextInt();
+        }
+
+        // Printing total_p and burst time table
+        System.out.println("-------------------------");
+		System.out.println("| P No. | AT\t| BT\t|");
+		System.out.println("-------------------------");
 		for (int i = 0; i < total_p; i++) {
-			pNo[i] = i + 1;
-			at[i] = sc.nextInt();
+			System.out.println("| P" + pNo[i] + "    | " + at[i] + "\t| " + bt[i] + "\t|");
 		}
+        System.out.println("-------------------------");
 
-		// getting Burst time from user
-		System.out.println("\nEnter Burst Time for all Processes: e.g. P1 P2 P3 P4 P5 (separate with spaces)");
-		for (int i = 0; i < total_p; i++) {
-			bt[i] = sc.nextInt();
-		}
-
-		// Printing total processes, arrival time and burst time in a table
-		System.out.println("\nP No | AT   |  BT");
-		System.out.println("------------------");
-		for (int i = 0; i < total_p; i++) {
-			System.out.println("P" + pNo[i] + "   | " + format(at[i])  + "   | " + format(bt[i]));
-		}	
-        System.out.println("------------------");
-	
-        System.out.println("\n- - * FCFS | First Come First Serve * - -");
+        System.out.println("\n- - - - * FCFS | First Come First Serve * - - - -");
 
         int temp;
-		for (int i = 0; i < total_p; i++) {
-			for (int j = 0; j < total_p; j++) {
-				// sorting all processes according to their arrival time
-                if(at[i] < at[j]) {
+        for (int i = 0; i < total_p; i++) {
+            for (int j = 0; j < total_p; j++) {
+                // sorting all processes according to their arrival time
+                if (at[i] < at[j]) {
                     temp = at[i];
                     at[i] = at[j];
                     at[j] = temp;
@@ -80,47 +77,47 @@ public class FCFS {
                     bt[i] = bt[j];
                     bt[j] = temp;
                 }
-			}
-		}
+            }
+        }
 
-		 // Printing solution table
-		System.out.println("\nP No | AT   | BT   | CT   | TAT  | WT");
-		System.out.println("---------------------------------------");
-		for (int i = 0; i < total_p; i++) {
+         // Printing solution table
+        System.out.println("-------------------------------------------------");
+		System.out.println("| P No. | AT\t| BT\t| CT\t| TAT\t| WT\t|");
+		System.out.println("-------------------------------------------------");
+        for (int i = 0; i < total_p; i++) {
 
             /* calculating completion time for each process
             for first process, completion time is equal to its burst time 
             but for reset processes, completion time is sum of 
             previous process' completion time and current process' burst time */
 
-			ct[i] = i == 0 ? bt[i] : (ct[i-1] < at[i] ? at[i] : ct[i-1])  + bt[i]; 
+            ct[i] = i == 0 ? bt[i] : (ct[i - 1] < at[i] ? at[i] : ct[i - 1]) + bt[i];
 
             // turn around time is the difference between completion time and arrival time
-			tat[i] = ct[i] - at[i];
+            tat[i] = ct[i] - at[i];
 
             // waiting time is the difference between turn around time and burst time
-			wt[i] = tat[i] - bt[i];
+            wt[i] = tat[i] - bt[i];
 
             /* printing all values in a tabular format
-            format() method is called to concat 0 to single digits (example: 1,2,3 as 01,02,03)
             to have a properly formatted table */
 
-			System.out.println("P" + pNo[i] + "   | " + format(at[i]) + "   | " + format(bt[i]) + "   | " + format(ct[i]) + "   | " + format(tat[i]) + "   | " + format(wt[i]));
-			
-		}
-		System.out.println("---------------------------------------");
+            System.out.println("| P" + pNo[i] + "    | " + at[i] + "\t| " + bt[i] + "\t| " + ct[i] + "\t| " + tat[i] + "\t| " + wt[i] + "\t|");
+
+        }
+        System.out.println("-------------------------------------------------");
 
         // average turn around time = sum of all turn around times / total number of processes
-		System.out.println("\nAverage Turnaround Time: " + (sum(tat) / total_p));
+        System.out.println("\nAverage Turnaround Time: " + (sum(tat) / total_p));
 
         // average waiting time = sum of all waiting times / total number of processes
-		System.out.println("Average Waiting Time: " + (sum(wt) / total_p));
+        System.out.println("Average Waiting Time: " + (sum(wt) / total_p));
 
         // schedule length = total time taken to complete all processes - first arrival time
-        System.out.println("Schedule Length: " + (ct[total_p-1] - at[0]));
+        System.out.println("Schedule Length: " + (ct[total_p - 1] - at[0]));
 
         // through put = no. of processes / schedule length (answer in float)
-        System.out.println("Throughput: " + (float)total_p / (ct[total_p-1] - at[0]));
+        System.out.println("Throughput: " + (float) total_p / (ct[total_p - 1] - at[0]));
 
     }
 }
--- input_output/FCFS.md
+++ input_output/FCFS.md
@@ -15,7 +15,7 @@ Consider the set of 5 processes whose arrival time and burst time are given belo
 | P5          | 4            | 5          |
 | P6          | 5            | 2          |
 
-# Input
+## Input
 
 Sample Input for above question
 
@@ -25,22 +25,22 @@ Sample Input for above question
 3 2 1 4 5 2
 ```
 
-# Output
+## Output
 
 Sample Output for the above question
 
 ```
-- - * FCFS | First Come First Serve * - -
-
-P No | AT   | BT   | CT   | TAT  | WT
----------------------------------------
-P1   | 00   | 03   | 03   | 03   | 00
-P2   | 01   | 02   | 05   | 04   | 02
-P3   | 02   | 01   | 06   | 04   | 03
-P4   | 03   | 04   | 10   | 07   | 03
-P5   | 04   | 05   | 15   | 11   | 06
-P6   | 05   | 02   | 17   | 12   | 10
----------------------------------------
+- - - - * FCFS | First Come First Serve * - - - -
+-------------------------------------------------
+| P No. | AT    | BT    | CT    | TAT   | WT    |
+-------------------------------------------------
+| P1    | 0     | 3     | 3     | 3     | 0     |
+| P2    | 1     | 2     | 5     | 4     | 2     |
+| P3    | 2     | 1     | 6     | 4     | 3     |
+| P4    | 3     | 4     | 10    | 7     | 3     |
+| P5    | 4     | 5     | 15    | 11    | 6     |
+| P6    | 5     | 2     | 17    | 12    | 10    |
+-------------------------------------------------
 
 Average Turnaround Time: 6.8333335
 Average Waiting Time: 4.0
@@ -48,42 +48,42 @@ Schedule Length: 17
 Throughput: 0.3529412
 ```
 
-# Console
+## Console
 
 This is what you get in your console after executing the program
 
 ```
 Enter Number of Processes: 6
 
-- - - -  * Enter Burst Time and Arrival Time for each Process * - - - -
+Enter Burst Time and Arrival Time for each Process
 
 Enter Arrival Time for all Processes: e.g. P1 P2 P3 P4 P5 (separate with spaces)
 0 1 2 3 4 5
 
 Enter Burst Time for all Processes: e.g. P1 P2 P3 P4 P5 (separate with spaces)
 3 2 1 4 5 2
+-------------------------
+| P No. | AT    | BT    |
+-------------------------
+| P1    | 0     | 3     |
+| P2    | 1     | 2     |
+| P3    | 2     | 1     |
+| P4    | 3     | 4     |
+| P5    | 4     | 5     |
+| P6    | 5     | 2     |
+-------------------------
 
-P No | AT   |  BT
-------------------
-P1   | 00   | 03
-P2   | 01   | 02
-P3   | 02   | 01
-P4   | 03   | 04
-P5   | 04   | 05
-P6   | 05   | 02
-------------------
-
-- - * FCFS | First Come First Serve * - -
-
-P No | AT   | BT   | CT   | TAT  | WT
----------------------------------------
-P1   | 00   | 03   | 03   | 03   | 00
-P2   | 01   | 02   | 05   | 04   | 02
-P3   | 02   | 01   | 06   | 04   | 03
-P4   | 03   | 04   | 10   | 07   | 03
-P5   | 04   | 05   | 15   | 11   | 06
-P6   | 05   | 02   | 17   | 12   | 10
----------------------------------------
+- - - - * FCFS | First Come First Serve * - - - -
+-------------------------------------------------
+| P No. | AT    | BT    | CT    | TAT   | WT    |
+-------------------------------------------------
+| P1    | 0     | 3     | 3     | 3     | 0     |
+| P2    | 1     | 2     | 5     | 4     | 2     |
+| P3    | 2     | 1     | 6     | 4     | 3     |
+| P4    | 3     | 4     | 10    | 7     | 3     |
+| P5    | 4     | 5     | 15    | 11    | 6     |
+| P6    | 5     | 2     | 17    | 12    | 10    |
+-------------------------------------------------
 
 Average Turnaround Time: 6.8333335
 Average Waiting Time: 4.0
